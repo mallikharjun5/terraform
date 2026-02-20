07a8cfb3b80390b6e926798b46a47ecf2955e178
mallikharjun <mallikharjunandey5@gmail.com>
2026-02-20 22:49:06 +0530

    variables

---
+++ variables/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "terraform" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow-all-ports.id]
+
+  tags = {
+    Name = "terraform"
+  }
+}
+
+resource "aws_security_group" "allow-all-ports" {
+    name = "allow-all-ports"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+     ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow-all-ports"
+    }
+}
