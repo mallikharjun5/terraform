5f1f036eb4d2868f5566695598a674c878587798
mallikharjun <mallikharjunandey5@gmail.com>
2026-02-21 20:33:45 +0530

    for loop

--- for-loop/ec2.tf
+++ for-loop/ec2.tf
@@ -1,11 +1,13 @@
 resource "aws_instance" "terraform" {
-  for_each = var.instances
+  #for_each = var.instances
+  for_each = toset(var.instances)
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = each.value
   vpc_security_group_ids = [aws_security_group.allow-all-ports.id]
 
   tags = {
-    Name = each.key
+    # Name = each.key
+    Name = each.value
   }
 }
 
