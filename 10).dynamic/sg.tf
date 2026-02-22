9c24635c17cd6fc549ff656a4b2c7d59167e2345
mallikharjun <mallikharjunandey5@gmail.com>
2026-02-22 18:02:17 +0530

    feb 22 dynamic block

--- 10).dynamic/sg.tf
+++ 10).dynamic/sg.tf
@@ -1,31 +1,3 @@
-resource "aws_security_group" "allow-all-ports" {
-    name = "allow-all-ports"
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    }
-
-     ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    }
-
-dynamic "ingress" {
-    for_each = toset(var.ingress_ports)
-    content {
-        from_port        = ingress.value
-        to_port          = ingress.value
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-    }
-    }
-
-    tags = {
-        Name = "allow-all-ports"
-    }
+variable "ingress_ports" {
+  default = [22,80,8080,3306,22017]
 }
